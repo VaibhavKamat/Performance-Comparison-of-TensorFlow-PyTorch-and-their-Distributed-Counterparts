6e603e706047775d990b4f05c862d14abb055b8b
vasudev-sridhar <vasudev.sridhar007@gmail.com>
2021-04-15 18:29:33 +0000

    temp pytorch code distributed

--- another_neural_net.py
+++ another_neural_net.py
@@ -2,10 +2,37 @@ import argparse
 import torch
 from torch import nn
 from torch import optim
+from torchvision import models
+from PIL import Image
+from torchvision import transforms
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data import DataLoader
+
+import os
+import numpy as np
+import glob
+import time
+
+def get_image_paths(root):
+  
+  count = 0
+  index = 0
+  image_paths = []
+  labels = []
+
+  for dir in os.listdir(root):
+    os.chdir(root+'/'+dir)
+    count += len(glob.glob('*.JPEG'))
+    labels += [index] * len(glob.glob('*.JPEG'))
+
+    for img in glob.glob('*.JPEG'):
+      img_path = root+'/'+dir+'/'+img
+      image_paths.append(img_path)
+      #print(img_path)
+  print(count)
+  return image_paths
 
 # prase the local_rank argument from command line for the current process
 parser = argparse.ArgumentParser()
@@ -38,37 +65,46 @@ else:
     device = torch.device("cpu", args.local_rank)
     print('No GPU. switching to CPU')
 
-model = nn.Sequential(nn.Linear(784, 128),
-                      nn.ReLU(),
-                      nn.Linear(128, 64),
-                      nn.ReLU(),
-                      nn.Linear(64, 10),
-                      nn.LogSoftmax(dim=1))
+def resnet50():
+    model = models.resnet50(pretrained=True)
+    model = model.to(device)
+    root = '/home/vasudev_sridhar007/project/Performance-Comparison-of-TensorFlow-PyTorch-and-their-Distributed-Counterparts/imagenette2/val'
+    image_paths = get_image_paths(root)
+    natural_img_dataset = datasets.ImageFolder(
+                              root = root
+                       )
+    with open('/home/vasudev_sridhar007/project/Performance-Comparison-of-TensorFlow-PyTorch-and-their-Distributed-Counterparts/imagenet1000_clsidx_to_labels.txt') as f:
+        labels = [line.strip() for line in f.readlines()]
+    
+    dist_sampler = DistributedSampler(natural_img_dataset)
+    trainloader = DataLoader(trainset, sampler=dist_sampler)
 
-model = model.to(device)
-# Define the loss
-criterion = nn.NLLLoss()
-# Optimizers require the parameters to optimize and a learning rate
-optimizer = optim.SGD(model.parameters(), lr=0.003)
-epochs = 5
-for e in range(epochs):
-    running_loss = 0
-    for images, labels in trainloader:
-        # Flatten MNIST images into a 784 long vector
-        images = images.view(images.shape[0], -1)
-        images = images.to(device)
-        labels = labels.to(device)
-        # Training pass
-        optimizer.zero_grad()
+    preprocess = transforms.Compose([
+        #transforms.Resize(259),
+        #transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        #transforms.Normalize(
+        #mean=[0.485, 0.456, 0.406],
+        #std=[0.229, 0.224, 0.225])
+        ])
+    t1 = time.time()
+    for img in trainloader:
+      #'/content/drive/MyDrive/Documents/CIFAR-10-images-master/ILSVRC2012_val_00037861.JPEG'
+      ele = Image.open(img).convert('RGB')
+      ele = preprocess(ele)
+      ele = torch.unsqueeze(ele, 0)
+      ele = ele.to(device)
+      preds = model(ele)
+      _, index = torch.max(preds, 1)
+      percentage = torch.nn.functional.softmax(preds, dim=1)[0] * 1000
+      print(labels[index[0]], percentage[index[0]].item())
 
-        output = model(images)
-        loss = criterion(output, labels)
-        loss.backward()
-        optimizer.step()
+    #_, indices = torch.sort(preds, descending=True)
+    #[(labels[idx], percentage[idx].item()) for idx in indices[0][:5]]
 
-        running_loss += loss.item()
-    else:
-        print(f"Training loss: {running_loss / len(trainloader)}")
+    print('Inference Time is: {} seconds'.format(time.time()-t1))
+
+resnet50()
 
 # python3 -m torch.distributed.launch --nproc_per_node=4 --nnodes=2 --node_rank=0 --master_addr="10.182.0.2" --master_port=1234 another_neural_net.py
 # python3 -m torch.distributed.launch --nproc_per_node=4 --nnodes=2 --node_rank=1 --master_addr="10.182.0.2" --master_port=1234 another_neural_net.py
